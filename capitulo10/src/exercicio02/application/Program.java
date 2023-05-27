650f2cf8b8c1450fbc7bd3154c94fd68edfd5361
Danillo Barcelos <danillofernandesb@gmail.com>
2023-05-27 11:48:18 -0300

    ex02 modulo10

---
+++ capitulo10/src/exercicio02/application/Program.java
@@ -0,0 +1,68 @@
+package exercicio02.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import exercicio02.entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.print("How many employees will be registered? ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Employee #" + i + ": ");
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			while (hasId(list, id)) {
+				System.out.print("This id is already taken. Please choose another one: ");
+				id = sc.nextInt();
+			}
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
+		}
+		
+		System.out.println("---------------");
+		
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if (emp == null) {
+			System.out.println("This id does not exist! Try again: ");
+		} else {
+			System.out.print("Please enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.IncreaseSalary(percentage);
+		}
+		
+		System.out.println("---------------");
+		
+		System.out.println("List of employees: ");
+		for(Employee i : list) {
+			System.out.println(i);
+		}
+		
+
+		sc.close();
+	}
+
+	private static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
+	}
+
+}
